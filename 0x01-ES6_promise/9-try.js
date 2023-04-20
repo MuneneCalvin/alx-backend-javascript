cd411bee14f2a2a78066653864a2f9d027724023
MuneneCalvin <munenecalvn@gmail.com>
2023-04-21 01:02:18 +0300

    fixed[Updated]

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,14 +1,12 @@
 export default function guardrail(mathFunction) {
   const queue = [];
-
   try {
-    queue.push(mathFunction());
-  } catch (err) {
-    queue.push(String(err));
+    const result = mathFunction();
+    queue.push(result);
+  } catch (error) {
+    queue.push(error.toString());
   } finally {
     queue.push('Guardrail was processed');
   }
-
   return queue;
 }
-
