9aa7f5a92a6a6896d47b1fc825e3ec0560443967
MuneneCalvin <munenecalvn@gmail.com>
2023-04-21 01:00:17 +0300

    fixed[Updated]

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,7 +1,8 @@
-export default function signUpUser(firstName, lastName) {
+function signUpUser(firstName, lastName) {
   return Promise.resolve({
     firstName,
     lastName,
   });
 }
 
+export default signUpUser;
