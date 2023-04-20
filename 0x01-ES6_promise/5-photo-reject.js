18784811af53e999c3e75f7ff6fbcecbc5d0c6d5
MuneneCalvin <munenecalvn@gmail.com>
2023-04-21 01:00:36 +0300

    fixed[Updated]

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,4 +1,5 @@
-export default function uploadPhoto(filename) {
-  return Promise.reject(new Error(`${filename} cannot be processed`));
+export default function uploadPhoto(fileName) {
+  return new Promise((resolve, reject) => {
+    reject(new Error(`${fileName} cannot be processed`));
+  });
 }
-
