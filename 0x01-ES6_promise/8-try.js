bcc110796d076b99be06081c3decaf1c1c9b1c16
MuneneCalvin <munenecalvn@gmail.com>
2023-04-21 01:01:42 +0300

    fixed[Updated]

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,8 +1,6 @@
 export default function divideFunction(numerator, denominator) {
   if (denominator === 0) {
-    throw Error('cannot divide by 0');
-  } else {
-    return numerator / denominator;
+    throw new Error('cannot divide by 0');
   }
+  return numerator / denominator;
 }
-
