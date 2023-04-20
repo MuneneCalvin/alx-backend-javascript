3b5fc3ad04cef58f96397566d04a02ac3f618bf9
MuneneCalvin <munenecalvn@gmail.com>
2023-04-21 00:59:59 +0300

    fixed[Updated]

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,13 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default function handleProfileSignup() {
-  const x = uploadPhoto();
-  const y = createUser();
-  return Promise.all([x, y]).then(
-    (data) => console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`),
-    () => console.log('Signup system offline'),
-  );
+function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()])
+    .then(([photo, user]) => {
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
 }
 
+export default handleProfileSignup;
