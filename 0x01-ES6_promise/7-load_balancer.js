d1d62b2a432a00fac905c6235be1016888b94de2
MuneneCalvin <munenecalvn@gmail.com>
2023-04-21 01:01:24 +0300

    fixed[Updated]

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,6 +1,3 @@
 export default function loadBalancer(chinaDownload, USDownload) {
-  return Promise
-    .race([chinaDownload, USDownload])
-    .then((res) => res);
+  return Promise.race([chinaDownload, USDownload]);
 }
-
