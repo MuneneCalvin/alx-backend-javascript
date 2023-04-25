482e6c294024dd9a25770f1afd468cebb9a11e25
MuneneCalvin <munenecalvn@gmail.com>
2023-04-25 17:44:38 +0300

    1. More mapping

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,16 +1,7 @@
-/**
- * Retrieves ids from a list of students.
- *  @param {{
- *   id: Number,
- *   firstName: String,
- *   location: String
- *  }[]} students - The list of students.
- *  @returns
- */
-
 export default function getListStudentIds(students) {
-    if (students instanceof Array) {
-        return students.map((student) => student.id);
-    }
+  if (!Array.isArray(students)) {
     return [];
+  }
+
+  return students.map((student) => student.id);
 }
