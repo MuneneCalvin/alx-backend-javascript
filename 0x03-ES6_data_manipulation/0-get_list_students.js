6d3b4c8ae6f16f47419ce33a10ca46a7ef907366
MuneneCalvin <munenecalvn@gmail.com>
2023-04-25 17:38:19 +0300

    0. Basic list of objects

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,12 +1,7 @@
-/**
- *  Retrieves a list of students.
- *  @returns {{id: Number, firstName: String, location: String}[]}
- */
-
 export default function getListStudents() {
-    return [
+  return [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Francisco' },
-    ];
+  ];
 }
