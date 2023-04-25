e1ce3da9a40390fe036a4f9ab204124cbac8a1c3
MuneneCalvin <munenecalvn@gmail.com>
2023-04-25 17:46:23 +0300

    3. Reduce

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,19 +1,3 @@
-/**
- * Retrieves the sum of ids of a list of students.
- *  @param {{
- *   id: Number,
- *   firstName: String,
- *   location: String
- *  }[]} students - The list of students.
- * @returns {Number}
- */
-
 export default function getStudentIdsSum(students) {
-    if (students instanceof Array) {
-        return students.reduce(
-            (prevStudent, curStudent) => prevStudent.id || prevStudent + curStudent.id,
-            0,
-        );
-    }
-    return 0;
+  return students.reduce((sum, student) => sum + student.id, 0);
 }
