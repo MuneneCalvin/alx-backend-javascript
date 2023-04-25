14d1c0e8b05386be00d8e6ffe8127ed00b30a7e3
MuneneCalvin <munenecalvn@gmail.com>
2023-04-25 17:51:19 +0300

    9. Map data structure

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,15 +1,9 @@
-/**
- * Retrieves a map of grocery names and their quantity.
- * @returns {Map<string, Number>}
- */
-
 export default function groceriesList() {
-    const values = [
-        ['Apples', 10],
-        ['Tomatoes', 10],
-        ['Pasta', 1],
-        ['Rice', 1],
-        ['Banana', 5],
-    ];
-    return new Map(values);
+  const groceries = new Map();
+  groceries.set('Apples', 10);
+  groceries.set('Tomatoes', 10);
+  groceries.set('Pasta', 1);
+  groceries.set('Rice', 1);
+  groceries.set('Banana', 5);
+  return groceries;
 }
