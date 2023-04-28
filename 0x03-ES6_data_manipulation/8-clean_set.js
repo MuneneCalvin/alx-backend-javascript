b7d966fded6121bed6b1bb1c74f765c96919647f
MuneneCalvin <munenecalvn@gmail.com>
2023-04-28 20:22:58 +0300

    [Fixed]  8. Clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,17 +1,15 @@
 export default function cleanSet(set, startString) {
-  const parts = [];
-  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
-    return '';
-  }
-  for (const value of set.values()) {
-    if (typeof value === 'string' && value.startsWith(startString)) {
-      const valueSubStr = value.substring(startString.length);
+  let result = '';
 
-      if (valueSubStr && valueSubStr !== value) {
-        parts.push(valueSubStr);
-      }
+  for (const value of set) {
+    if (value.startsWith(startString)) {
+      result += `${value.substring(startString.length)}-`;
     }
   }
-  return parts.join('-');
-}
 
+  if (result.length > 0) {
+    result = result.substring(0, result.length - 1);
+  }
+
+  return result;
+}
