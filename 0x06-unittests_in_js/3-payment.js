d0cbeba0e2052c2e09b2b4dd8c0ed983a832e727
MuneneCalvin <munenecalvn@gmail.com>
2023-06-16 10:25:25 +0300

    0x06. Unittests in JS

--- 0x06-unittests_in_js/3-payment.js
+++
@@ -1,8 +0,0 @@
-const Utils = require('./utils');
-
-const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
-  const totalCost = Utils.calculateNumber('SUM', totalAmount, totalShipping);
-  console.log(`The total is: ${totalCost}`);
-};
-
-module.exports = sendPaymentRequestToApi;
--- 0x06-unittests_in_js/6-payment_token.js
+++
@@ -1,7 +0,0 @@
-const getPaymentTokenFromAPI = (success) => new Promise((resolve, _reject) => {
-  if (success) {
-    resolve({ data: 'Successful response from the API' });
-  }
-});
-
-module.exports = getPaymentTokenFromAPI;
